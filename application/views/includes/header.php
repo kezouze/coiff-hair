305ae28b57187ae334a7a65247bcadb7cc51cb37
kezouze <vincent-c51@hotmail.fr>
2023-08-26 20:02:41 +0200

    🎨 Now this is good

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,7 +1,11 @@
     <div class="pages_header">
-        <?php include(APPPATH . 'views/includes/small_icon.php'); ?>
+        <span style="width:33%">
+            <?php include(APPPATH . 'views/includes/small_icon.php'); ?>
+        </span>
         <h1 style="color:<?= $color ?>" class="titre"><?= $title ?></h1>
-        <?php if (isset($add)) {
-            echo $add;
-        } ?>
+        <span style="width:33%">
+            <?php if (isset($add)) {
+                echo $add;
+            } ?>
+        </span>
     </div>
