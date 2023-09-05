720b0dcdc9fb4ec90aa3f4a2dcdfadc5dfe674cf
kezouze <vincent-c51@hotmail.fr>
2023-09-05 21:49:50 +0200

    🍻 🚧 Good night

--- application/views/espace_salons/info_prestations.php
+++ application/views/espace_salons/info_prestations.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <?php
 $title = $name . ' - Prestations';
 $color = "#2f4f4f";
@@ -10,16 +11,19 @@ include(APPPATH . '/views/includes/head.php');
 <body>
     <div class="blur">
         <?php include(APPPATH . '/views/includes/header.php'); ?>
-        <!-- <div class="presta_container"> -->
-        <?php foreach ($all_prestas as $key) { ?>
-            <div class="presta_card">
-                <h3><?= $key->presta_name ?></h3>
-                <p><?= $key->presta_descr ?></p>
-                <p><?= str_replace('.', ',', $key->presta_cost) ?>€</p>
-                <p></p>
-            </div>
-        <?php } ?>
-        <!-- </div> -->
+        <div class="presta_container">
+            <?php foreach ($all_prestas as $key) {
+                if (count($all_prestas) > 0) { ?>
+                    <div class="presta_card">
+                        <h3><?= $key->presta_name ?></h3>
+                        <p><?= $key->presta_descr ?></p>
+                        <p><?= str_replace('.', ',', $key->presta_cost) ?>€</p>
+                    </div>
+                <?php } else { ?>
+                    <p>Revenez bientôt pour des nouveautés !</p>
+            <?php }
+            } ?>
+        </div>
         <?php include(APPPATH . '/views/includes/footer.php'); ?>
     </div>
 </body>
