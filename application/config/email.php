045f0087935da54f448dd299f2598c7df2e7cddb
kezouze <vincent-c51@hotmail.fr>
2023-09-08 14:01:25 +0200

    🤡 still trying smtp configuration

--- application/config/email.php
+++ application/config/email.php
@@ -1,12 +1,62 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-// Marche plus 
+// Marche pas
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'mail.infomaniak.com';
 $config['smtp_user'] = 'projet-pro@outil-web.fr';
 $config['smtp_pass'] = 'TSA-corp69';
 $config['smtp_port'] = 465;
+$config['smtp_crypto'] = 'starttls';
 // $config['smtp_crypto'] = 'tls';
 $config['mailtype'] = 'html';
 $config['charset'] = 'utf-8';
+
+// Marche pas
+/*
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'smtp.office365.com';
+$config['smtp_user'] = 'vincent-c51@hotmail.fr';
+$config['smtp_pass'] = $mdp;
+$config['smtp_port'] = 587;
+$config['smtp_crypto'] = 'starttls';
+$config['mailtype'] = 'html';
+$config['charset'] = 'utf-8';
+*/
+
+// Marche pas
+/*
+$config['protocol'] = 'IMAP';
+$config['smtp_host'] = 'outlook.office365.com';
+$config['smtp_user'] = 'vincent-c51@hotmail.fr';
+$config['smtp_pass'] = $mdp;
+$config['smtp_port'] = 993;
+$config['smtp_crypto'] = 'TLS';
+$config['mailtype'] = 'html';
+$config['charset'] = 'utf-8';
+*/
+
+// Marche pas
+/*
+$config['protocol'] = 'POP';
+$config['smtp_host'] = 'outlook.office365.com';
+$config['smtp_user'] = 'vincent-c51@hotmail.fr';
+$config['smtp_pass'] = $mdp;
+$config['smtp_port'] = 995;
+$config['smtp_crypto'] = 'TLS';
+$config['mailtype'] = 'html';
+$config['charset'] = 'utf-8';
+*/
+
+
+// configure the email setting
+/*
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = '';
+$config['smtp_user'] = '';
+$config['smtp_pass'] = $mdp;
+$config['smtp_port'] = 465;
+$config['smtp_crypto'] = 'ssl';
+$config['mailtype'] = 'html';
+$config['charset'] = 'utf-8';
+*/
