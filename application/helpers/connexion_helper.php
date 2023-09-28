54ad42f69ae1981cc1203260617c2c9095314706
kezouze <vincent-c51@hotmail.fr>
2023-09-28 08:54:18 +0200

    🎨 better isConnected function

--- application/helpers/connexion_helper.php
+++ application/helpers/connexion_helper.php
@@ -1,12 +1,15 @@
 <?php
-// les helpers doivent porter l'extension _helper.php mais sont appelés sans dans l'autoload // 
+// les helpers doivent porter l'extension _helper.php mais sont appelés sans dans l'autoload
 session_start();
 
 function isConnected()
 {
-    if (isset($_SESSION['pseudo']) || isset($_SESSION['id'])) {
-        return true;
-    } else {
-        return false;
-    }
+    // if (isset($_SESSION['pseudo']) || isset($_SESSION['id'])) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+    // beaucoup plus simple :
+    return isset($_SESSION['pseudo']) || isset($_SESSION['id']);
 }
