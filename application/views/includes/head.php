3b07ba89af9fd1c0cbfcc90a67f3c82ce28dda69
kezouze <vincent-c51@hotmail.fr>
2025-02-19 18:10:39 +0100

    base_url() to access the good css and js files path in <head>

--- application/views/includes/head.php
+++ application/views/includes/head.php
@@ -2,10 +2,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/coiffhair/assets/css/style.css">
     <title><?= $title ?></title>
-    <!-- <link rel="icon" href="/coiffhair/assets/images/ciseaux.png"> -->
-     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="icon" href="<?=base_url('assets/images/ciseaux.png') ?>">
+     <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="assets/js/custom.js"></script>
+    <script src="<?=base_url('assets/js/custom.js') ?>"></script>
 </head>
